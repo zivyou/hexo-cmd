09a86addd1cc3d0b0143710971dc209ed23f4e9e
zivyou <yzq529@qq.com>
2019-05-05 14:38:54 +0800

    first trial

--- extension.js
+++ extension.js
@@ -1,6 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+var exec = require("child_process").exec;
+var hexo_cli = require("hexo-cli");
+var Hexo = require("hexo");
+var hexo = new Hexo(process.cwd(), {});
+var minimist = require('minimist');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,14 +22,32 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+	let hexo_g = vscode.commands.registerCommand('extension.hexo.hexo-generate', function () {
 		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		hexo_cli(process.cwd(), minimist(["generate"])).then(()=>{
+			
+		});
 	});
 
-	context.subscriptions.push(disposable);
+	let hexo_d = vscode.commands.registerCommand('extension.hexo.hexo-deploy', function () {
+		// The code you place here will be executed every time your command is executed
+		hexo_cli(process.cwd(), minimist(["deploy"])).then(()=>{
+
+		});
+	});
+
+	let hexo_init = vscode.commands.registerCommand('extension.hexo.hexo-init', function () {
+		// The code you place here will be executed every time your command is executed
+		hexo.init().then(data=>{
+			console.log("=======================");
+		}).catch(err=>{
+			console.log("2=======================");
+		});	
+	});
+
+	context.subscriptions.push(hexo_g);
+	context.subscriptions.push(hexo_d);
+	context.subscriptions.push(hexo_init);
 }
 exports.activate = activate;
 
